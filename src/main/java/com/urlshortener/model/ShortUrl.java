54d28f22e5bdeb2455fd81e3fc63221fb2260473
zenith-11 <samalabhinash351@gmail.com>
2026-02-05 14:52:43 +0530

    created model short_url table

---
+++ src/main/java/com/urlshortener/model/ShortUrl.java
@@ -0,0 +1,31 @@
+package com.urlshortener.model;
+
+import jakarta.persistence.*;
+
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "short_urls")
+public class ShortUrl {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "short_code", nullable = false, unique = true, length = 10)
+    private String shortCode;
+
+    @Column(name = "long_url", nullable = false, columnDefinition = "TEXT")
+    private String longUrl;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "expires_at")
+    private LocalDateTime expiresAt;
+
+    @Column(name = "click_count", nullable = false)
+    private Long clickCount;
+
+
+}
