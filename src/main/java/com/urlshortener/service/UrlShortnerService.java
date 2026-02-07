3cc2df1af634c447a2efed0aa2e24dea0e622787
zenith-11 <samalabhinash351@gmail.com>
2026-02-07 16:23:13 +0530

    Add Redis caching for redirect hot path

--- src/main/java/com/urlshortener/service/UrlShortnerService.java
+++ src/main/java/com/urlshortener/service/UrlShortnerService.java
@@ -5,15 +5,22 @@ import com.urlshortener.Dto.CreateShortUrlResponse;
 import com.urlshortener.model.ShortUrl;
 import com.urlshortener.repository.ShortUrlRepository;
 import com.urlshortener.util.ShortCodeGenerator;
+import org.springframework.stereotype.Service;
+import java.util.Optional;
+import org.springframework.data.redis.core.StringRedisTemplate;
 
 import java.time.LocalDateTime;
 
+@Service
 public class UrlShortnerService {
 
     private final ShortUrlRepository shortUrlRepository;
+    private final StringRedisTemplate redisTemplate;
 
-    public UrlShortnerService(ShortUrlRepository shortUrlRepository) {
+
+    public UrlShortnerService(ShortUrlRepository shortUrlRepository,  StringRedisTemplate redisTemplate) {
         this.shortUrlRepository = shortUrlRepository;
+        this.redisTemplate = redisTemplate;
     }
 
     public CreateShortUrlResponse createShortUrl(CreateShortUrlRequest request){
@@ -38,4 +45,35 @@ public class UrlShortnerService {
 
         return new CreateShortUrlResponse("http://localhost:8080/" + shortcode);
     }
+
+    public String resolveShortUrl(String shortCode) {
+
+        String cacheKey = "shorturl:" + shortCode;
+        String cachedUrl = redisTemplate.opsForValue().get(cacheKey);
+        if (cachedUrl != null) {
+            return cachedUrl; // 🚀 fast path
+        }
+
+        ShortUrl shortUrl = shortUrlRepository
+                .findByShortCode(shortCode)
+                .orElseThrow(() -> new RuntimeException("Short URL not found"));
+
+        // check expiry
+        if (shortUrl.getExpiresAt() != null &&
+                shortUrl.getExpiresAt().isBefore(LocalDateTime.now())) {
+            throw new RuntimeException("Short URL expired");
+        }
+
+        redisTemplate.opsForValue()
+                .set(cacheKey, shortUrl.getLongUrl());
+
+        // increment click count
+        shortUrl.setClickCount(shortUrl.getClickCount() + 1);
+        shortUrlRepository.save(shortUrl);
+
+        return shortUrl.getLongUrl();
+    }
+
+
+
 }
